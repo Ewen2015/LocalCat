51cb4ab98b5afb38efeaade1e82c31202173ebf5
Ewen Wang <wang.enqun@outlook.com>
2023-11-27 11:26:03 +0800

    Update ssh-key-gen.sh

--- ssh-key-gen.sh
+++ ssh-key-gen.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-cd ~/.ssh
-ssh-keygen
-cat id_rsa.pub
+# Generate a new SSH key and add it to your machine's SSH agent
+ssh-keygen -t ed25519 -C "your email"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+
+cat ~/.ssh/id_ed25519.pub
+
+# Adding a new SSH key to your account
