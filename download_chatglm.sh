adda06221ec3627f315710d73b0713c9ca16df46
Ewen2015 <wang.enqun@outlook.com>
2024-02-07 18:31:13 +0800

    download models from modelscope

--- download_chatglm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for ((i=1; i<=7; i++))
-do
-    filename="pytorch_model-$(printf "%05d" $i)-of-00007.bin"
-    url="https://cloud.tsinghua.edu.cn/seafhttp/files/22c6f3eb-6830-4723-851d-f11551b47df5/$filename"
-    wget "$url"
-done
---
+++ download_model.py
@@ -0,0 +1,2 @@
+from modelscope import snapshot_download
+snapshot_download('ZhipuAI/chatglm3-6b', revision = "v1.0.0", cache_dir="./models/")
