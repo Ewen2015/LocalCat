112c35c742d75673d2c6b305a893b162034f02e5
Ewen2015 <wang.enqun@outlook.com>
2024-02-28 14:15:57 +0800

    app demo

--- app/demo.py
+++ app/demo.py
@@ -8,8 +8,7 @@ license:    Apache License 2.0
 
 import streamlit as st
 import pandas as pd
-# from LocalCat.Translate import Translate
-from Translate import Translate
+from LocalCat.Translate import Translate
 
 st.set_page_config(
      page_title="LLM Translator",
@@ -17,58 +16,71 @@ st.set_page_config(
      initial_sidebar_state=st.session_state.get("sidebar_state", "collapsed")
      )
 
+model_name = f"mbart-finetuned-cn-to-en-auto"
+model_path = f"../models/{model_name}"
+
 st.title("LLM Translator with AI In-house")
+
+with st.expander("Pain Points to Solve"):
+    st.write("""
+    1. High cost of translation services. (1 RMB/item and we have a large and increasing customer base in China)
+    2. Inaccurate translation in industrial and brand-specific scenarios. For instance, '窝窝' is a nickname for Volvo Cars, coined by Volvo customers in China.
+    3. Difficulty transferring knowledge when transitioning to a new translation service.
+    """)
 
 tab1, tab2, tab3 = st.tabs(["Translator", "Fine-tune", "New Request"])
 with tab1:
-
-    try:
-        model_name = f"mbart-finetuned-cn-to-en-auto"
-        model_path = f"../models/{model_name}"
-        st.info("Loading the model. This can take a while.")
-        trans = Translate(model_path)
-        model_ready = True
-    except:
-        st.warnings("The model is not available. Please check the model path.")
-        model_ready = False
-
+    st.write("Here, you can translate your text by case or on batch.")
     tab11, tab12 = st.tabs(["Chat", "Batch"])
     with tab11:
-        text = st.text_input('Input')
+        st.write("###### Text to translate (Chinese)")
+        text = st.text_input(label="Input", value=None)
         if text is not None:
-            if model_ready:
-                result = trans.translator(text)
-            else:
-                result = "The model is not available. Please check the model path."
-        st.write("Translation")
+            st.info("Loading the model and translating. Take a moment.")
+            trans = Translate(model_path)
+            result = trans.translator(text)
+        else:
+            result = "You will see the translation here."
+        st.write("###### Translation (English)")
         st.code(result)
     with tab12:
-        uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+        uploaded_file = st.file_uploader("Choose a CSV file to translate", type="csv")
         
         if uploaded_file is not None:
             df = pd.read_csv(uploaded_file)
-            if model_ready:
-                if st.button('Translate!'):
-                    df = trans.translator_batch(df, col_tgt="Translation")
-            else:
-                st.warnings("The model is not available. Please check the model path.")
-            
-            st.write("### DataFrame")
+            st.write("### Data to Translate")
             st.dataframe(df)
-            st.download_button(
-                label="Download data as CSV",
-                data=df.to_csv().encode('utf-8'),
-                file_name='LLM_TRANSLATOR_RESULTS.csv',
-                mime='text/csv',
-            )
+
+            if st.button('Translate'):
+                st.info(f"Loading the model. Take a coffee break.☕️")
+                trans = Translate(model_path)
+                st.info(f"Translating. Take a coffee break.☕️")
+                df = trans.translator_batch(df, col_tgt="Translation")
+            
+                st.write("### Translation Results")
+                st.dataframe(df)
+                st.download_button(
+                    label="Download data as CSV",
+                    data=df.to_csv().encode('utf-8'),
+                    file_name='LLM_TRANSLATOR_RESULTS.csv',
+                    mime='text/csv',
+                )
 
 with tab2:
-    st.write("This section allows users to fine-tune the model with their own data.")
-    st.write("We are working on it. Please stay tuned.📬")
+    st.write("Here, you can fine-tune the model with your own data.")
+    st.image('finetune.png', caption='LLM Fine-tunes (Source: https://abacus.ai/llmapi)')
+    st.write("Your data should contains two columns: `Chinese` and `English`. Each row is a new example for the machine to learn (so-called **machine learning**).")
+    
+    uploaded_file = st.file_uploader("Choose a CSV file to fine-tune", type="csv")
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        st.write("### Data for Fine-tuning")
+        st.dataframe(df)
 
 with tab3:
-    st.write("This section allows users to request new features for translation.")
-    st.write("We are working on it. Please stay tuned.📬")
+    st.write("Here, you can make requests to the translator based on the **latest terminology**.")
+    st.write("See the following two as examples.")
+    st.write("")
 
     df = pd.DataFrame(
         [
